dd4decf5147d32f6cdbab40fc44f6b566ce0624b
nitin doke <nitin123.doke@gmail.com>
2026-02-27 17:46:19 +0530

    Catalog.API project and basic structure of solution checkin into git.

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price
+        
+    ) : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler(IDocumentSession session) 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // create product entity from command object
+            // save to database
+            // return CreateProduct result
+
+            var product = new Product
+            {  
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                Price = command.Price,
+                ImageFile = command.ImageFile
+            };
+
+            //save to database
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            //return result
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+
+}).UseLightweightSessions();
+
+var app = builder.Build();
+
+// configure the http request pipeline.
+app.MapCarter();
+
+app.Run();
